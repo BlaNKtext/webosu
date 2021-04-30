cb1811aabd3a02019d2b347df3d96f22ce3944a1
Noah <blanktextt@gmail.com>
2021-04-30 20:39:15 +0000

    I cant figure this out lmao

--- scripts/overlay/score.js
+++ scripts/overlay/score.js
@@ -251,7 +251,7 @@ define([], function () {
 
         function uploadScore(summary) {
             let xhr = new XMLHttpRequest();
-            let url = "https://3001-magenta-cockroach-8wmgi1ls.ws-us04.gitpod.io/send/";
+            let url = "https://3000-magenta-cockroach-8wmgi1ls.ws-us04.gitpod.io/send/";
             url += "?sid=" + encodeURIComponent(summary.sid);
             url += "&bid=" + encodeURIComponent(summary.bid);
             url += "&title=" + encodeURIComponent(summary.title);
