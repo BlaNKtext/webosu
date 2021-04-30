17333307a56190bf59584c0b5480aacb93347256
Noah <blanktextt@gmail.com>
2021-04-30 20:13:45 +0000

    Change API url

--- scripts/overlay/score.js
+++ scripts/overlay/score.js
@@ -251,7 +251,7 @@ define([], function () {
 
         function uploadScore(summary) {
             let xhr = new XMLHttpRequest();
-            let url = "https://webosuapi.herokuapp.com/send/";
+            let url = "https://3001-magenta-cockroach-8wmgi1ls.ws-us04.gitpod.io/send/";
             url += "?sid=" + encodeURIComponent(summary.sid);
             url += "&bid=" + encodeURIComponent(summary.bid);
             url += "&title=" + encodeURIComponent(summary.title);
