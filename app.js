943bb8d6a49a94de75282d7e69a9773e4bdf65b3
BlaNK <sh1ttycoder@gmail.com>
2021-09-21 16:07:18 -0400

    compact app.js cuz why not

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8080;
 app.use("/", express.static(__dirname + "/public"));
-app.listen(port, () => {
-  console.log(`Main server running at http://localhost:${port}/`);
+app.listen(8080, () => {
+  console.log(`Main server running at http://localhost:8080/`);
 });
