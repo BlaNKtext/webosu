fb0cedf4c324474a5f2b2838daa602bc6db5b75a
Noah <blanktextt@gmail.com>
2023-01-08 11:56:46 -0500

    Download BM without video, AKA faster DL times

--- js/downloader.js
+++ js/downloader.js
@@ -45,7 +45,7 @@ function startdownload(box) {
     if (box.downloading) {
         return;
     }
-    let url = "https://catboy.best/d/" + box.sid;
+    let url = "https://catboy.best/d/" + box.sid + "n";
     box.downloading = true;
     box.classList.add("downloading");
     let xhr = new XMLHttpRequest();
