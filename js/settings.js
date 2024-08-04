57421747fb42ee65627b5392e830d940a5f17df8
BlaNKtext <blanktextt@gmail.com>
2024-08-04 17:56:19 -0400

    ball

--- js/settings.js
+++ js/settings.js
@@ -333,15 +333,3 @@ function setOptionPanel() {
 }
 
 window.addEventListener('DOMContentLoaded', setOptionPanel);
-
-
-//press any key to search
-window.onkeydown = function(e) {
-	if (e.ctrlKey || e.altKey || e.metaKey)
-		return;
-	if (e.key.length == 1 && e.key != " ") {
-		let textinput = document.getElementsByTagName("input")[0];
-		textinput.focus();
-	}
-}
-
